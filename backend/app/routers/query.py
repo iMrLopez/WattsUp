c66617a047ad456ce7da090a8a759a5115c41de6
iMrLopez <8272737+iMrLopez@users.noreply.github.com>
2025-03-06 21:01:27 -0600

    format & dockerize backend

---
+++ backend/app/routers/query.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+from core.prolog_engine import query_prolog, add_prolog
+from schemas import FactInput
+
+router = APIRouter()
+
+@router.get("/")
+def query(rule: str, args: str):
+    arg_list = args.split(",")  # Expect comma-separated values.
+    results = query_prolog(rule, *arg_list)
+    return {"result": bool(results), "data": results}
+
+@router.post("/add_fact")
+def add_fact(fact_data: FactInput):
+    return add_prolog(fact_data)
